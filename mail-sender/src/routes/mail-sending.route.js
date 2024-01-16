d0ccbcbb6046ace75cea5c17ff079846e0838c2f
King-Hin Leung <kinghin.leung@commercetools.com>
2024-01-16 17:28:03 +0100

    Update mail-sending.route.js

--- mail-sender/src/routes/mail-sending.route.js
+++ mail-sender/src/routes/mail-sending.route.js
@@ -3,6 +3,9 @@ import { Router } from 'express';
 import { messageHandler } from '../controllers/mail-sending.controller.js';
 const eventRouter = Router();
 
-eventRouter.post('/mailSender', messageHandler);
+eventRouter.post('/mailSender',
+
+
+    messageHandler);
 
 export default eventRouter;
