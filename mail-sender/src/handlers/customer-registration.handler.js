b0a86e4775e67f662eabc146806b7641fb7f2cb7
King-Hin Leung <kinghin.leung@commercetools.com>
2024-01-18 17:38:31 +0100

    Send email with template via SendGrid

--- mail-sender/src/handlers/customer-registration.handler.js
+++ mail-sender/src/handlers/customer-registration.handler.js
@@ -1,12 +1,31 @@
 import { getCustomerById } from '../client/query.client.js';
 import GenericHandler from '../handlers/generic.handler.js';
+import SendGridMail from '@sendgrid/mail';
+import CustomError from '../errors/custom.error.js';
 class CustomerRegistrationHandler extends GenericHandler {
   constructor() {
     super();
   }
 
-  async sendMail() {
-    // TODO : Implement integration to external email service provider
+  async sendMail(email) {
+    SendGridMail.setApiKey(process.env.EMAIL_PROVIDER_API_KEY);
+    const msg = {
+      to: email,
+      from: 'kinghing.leung@commercetools.com', // Provided by user
+      templateId: 'd-dea1ed619a874fd88501535f6ebfee61', // Provided by user
+    };
+    try {
+      await SendGridMail.send(msg);
+    } catch (err) {
+      const statusCode = err?.code;
+      const errors = JSON.stringify(err.response?.body?.errors);
+
+      throw new CustomError(
+        statusCode,
+        'Fail to communicate with SendGrid.',
+        errors
+      );
+    }
   }
 
   async process(messageBody) {
