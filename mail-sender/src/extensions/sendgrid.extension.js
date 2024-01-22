674587e505c57dc311479489044270bd4c116850
King-Hin Leung <kinghin.leung@commercetools.com>
2024-01-22 10:09:00 +0100

    Refactor the customer params into customerDetails object

--- mail-sender/src/extensions/sendgrid.extension.js
+++ mail-sender/src/extensions/sendgrid.extension.js
@@ -1,28 +1,19 @@
 import SendGridMail from '@sendgrid/mail';
 import CustomError from '../errors/custom.error.js';
 
-export async function send(
-  customerEmail,
-  customerNumber,
-  customerFirstName,
-  customerLastName,
-  customerMiddleName,
-  customerCreationTime,
-  senderEmailAddress,
-  templateId
-) {
+export async function send(senderEmailAddress, templateId, customerDetails) {
   try {
     SendGridMail.setApiKey(process.env.EMAIL_PROVIDER_API_KEY);
     const msg = {
-      to: customerEmail,
+      to: customerDetails.customerEmail,
       from: senderEmailAddress,
       templateId,
       dynamicTemplateData: {
-        firstName: customerFirstName,
-        lastName: customerLastName,
-        middleName: customerMiddleName,
-        customerNumber: customerNumber,
-        creationTime: customerCreationTime,
+        firstName: customerDetails.customerFirstName,
+        lastName: customerDetails.customerLastName,
+        middleName: customerDetails.customerMiddleName,
+        customerNumber: customerDetails.customerNumber,
+        creationTime: customerDetails.customerCreationTime,
       },
     };
     await SendGridMail.send(msg);
--- mail-sender/src/handlers/customer-registration.handler.js
+++ mail-sender/src/handlers/customer-registration.handler.js
@@ -8,34 +8,32 @@ class CustomerRegistrationHandler extends GenericHandler {
     super();
   }
 
+  async sendMail(senderEmailAddress, templateId, customerDetails) {
+    await send(senderEmailAddress, templateId, customerDetails);
+  }
+
   async process(messageBody) {
     const senderEmailAddress = process.env.SENDER_EMAIL_ADDRESS;
     const templateId = process.env.CUSTOMER_REGISTRATION_TEMPLATE_ID;
 
     const customerId = messageBody.resource.id;
     const customer = await getCustomerById(customerId);
-    const customerEmail = customer.email;
-    const customerNumber = customer.customerNunmber;
-    const customerFirstName = customer.firstName;
-    const customerMiddleName = customer.middleName;
-    const customerLastName = customer.lastName;
-    const customerCreationTime = customer.createdAt;
+
+    const customerDetails = {
+      customerEmail: customer.email,
+      customerNumber: customer.customerNumber ? customer.customerNumber : '',
+      customerFirstName: customer.firstName ? customer.firstName : '',
+      customerMiddleName: customer.middleName ? customer.middleName : '',
+      customerLastName: customer.lastName ? customer.lastName : '',
+      customerCreationTime: customer.createdAt,
+    };
 
     logger.info(
-      `Ready to send confirmation email of customer registration : customerEmail=${customerEmail}, customerNumber=${customerNumber}, customerFirstName=${customerFirstName}, customerLastName=${customerLastName}, customerMiddleName=${customerMiddleName}, customerCreationTime=${customerCreationTime} `
+      `Ready to send confirmation email of customer registration : customerEmail=${customerDetails.customerEmail}, customerNumber=${customerDetails.customerNumber}, customerFirstName=${customerDetails.customerFirstName}, customerLastName=${customerDetails.customerLastName}, customerMiddleName=${customerDetails.customerMiddleName}, customerCreationTime=${customerDetails.customerCreationTime} `
     );
-    await send(
-      customerEmail,
-      customerNumber,
-      customerFirstName,
-      customerLastName,
-      customerMiddleName,
-      customerCreationTime,
-      senderEmailAddress,
-      templateId
-    );
+    await this.sendMail(senderEmailAddress, templateId, customerDetails);
     logger.info(
-      `Confirmation email of customer registration has been sent to ${customerEmail}.`
+      `Confirmation email of customer registration has been sent to ${customerDetails.customerEmail}.`
     );
   }
 }
