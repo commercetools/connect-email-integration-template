acc46ec69fb4a2f77bcb94599f74869936c033e3
King-Hin Leung <kinghin.leung@commercetools.com>
2024-02-01 17:23:02 +0100

    Remove provider-specific SDK invocation

--- mail-sender/src/handlers/generic.handler.js
+++ mail-sender/src/handlers/generic.handler.js
@@ -1,6 +1,6 @@
 import CustomError from '../errors/custom.error.js';
 import { HTTP_STATUS_SERVER_ERROR } from '../constants/http-status.constants.js';
-import { send } from '../extensions/sendgrid.extension.js';
+import { logger } from '../utils/logger.utils.js';
 
 class GenericHandler {
   async sendMail(
@@ -9,12 +9,11 @@ class GenericHandler {
     templateId,
     templateData
   ) {
-    await send(
-      senderEmailAddress,
-      recipientEmailAddress,
-      templateId,
-      templateData
-    );
+    logger.info(`senderEmailAddress: ${senderEmailAddress}`);
+    logger.info(`recipientEmailAddress: ${recipientEmailAddress}`);
+    logger.info(`templateId: ${templateId}`);
+    logger.info(`templateData: ${templateData}`);
+    // Implement the invocation to SDK supplied by email service provider
   }
 
   process() {
