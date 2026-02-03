a146a688246ad5ba2c2c6798c6c416931fe628a2
Dario Ocles <Dario Ocles>
2026-02-03 17:52:31 +0100

    chore: adding AWS support to event applications

--- mail-sender/src/connector/actions.js
+++ mail-sender/src/connector/actions.js
@@ -4,6 +4,8 @@ import {
   CUSTOMER_PASSWORD_TOKEN_SUBSCRIPTION_MESSAGE_TYPES,
   ORDER_SUBSCRIPTION_MESSAGE_TYPES,
 } from '../constants/constants.js';
+import { assertNonNullable } from '../utils/assert.utils.js';
+
 const EMAIL_DELIVERY_SUBSCRIPTION_KEY =
   'ct-connect-email-delivery-subscription';
 
@@ -35,6 +37,57 @@ export async function deleteEmailDeliverySubscription(apiRoot) {
       })
       .execute();
   }
+}
+
+function buildDestination(config) {
+  assertNonNullable(
+    config.connectSubscriptionDestination,
+    'CONNECT_SUBSCRIPTION_DESTINATION is required'
+  );
+
+  switch (config.connectSubscriptionDestination) {
+    case 'GCP':
+      assertNonNullable(config.connectGcpTopicName, 'CONNECT_GCP_TOPIC_NAME is required for GCP destination');
+      assertNonNullable(config.connectGcpProjectId, 'CONNECT_GCP_PROJECT_ID is required for GCP destination');
+      return {
+        type: 'GoogleCloudPubSub',
+        topic: config.connectGcpTopicName,
+        projectId: config.connectGcpProjectId,
+      };
+    case 'SNS':
+      assertNonNullable(config.connectAwsTopicArn, 'CONNECT_AWS_TOPIC_ARN is required for SNS destination');
+      return {
+        type: 'SNS',
+        topicArn: config.connectAwsTopicArn,
+        authenticationMode: 'IAM',
+      };
+    default:
+      throw new Error(
+        `Unsupported subscription destination: ${config.connectSubscriptionDestination}. Valid options are 'GCP' or 'SNS'.`
+      );
+  }
+}
+
+export async function createSubscription(apiRoot, config) {
+  await deleteEmailDeliverySubscription(apiRoot);
+
+  const destination = buildDestination(config);
+
+  await apiRoot
+    .subscriptions()
+    .post({
+      body: {
+        key: EMAIL_DELIVERY_SUBSCRIPTION_KEY,
+        destination,
+        messages: [
+          buildCustomerChangeMessageType(),
+          buildCustomerEmailTokenChangeMessageType(),
+          buildCustomerPasswordTokenChangeMessageType(),
+          buildOrderChangeMessageType(),
+        ],
+      },
+    })
+    .execute();
 }
 
 export async function createEmailDeliverySubscripition(
--- mail-sender/src/utils/assert.utils.js
+++ mail-sender/src/utils/assert.utils.js
@@ -11,3 +11,10 @@ export function assertError(value, message) {
 export function assertString(value, message) {
   assert(typeof value === 'string', message ?? 'Invalid string value');
 }
+
+export function assertNonNullable(value, message) {
+  assert(
+    value !== undefined && value !== null && value !== '',
+    message ?? 'Value must not be null or undefined'
+  );
+}
