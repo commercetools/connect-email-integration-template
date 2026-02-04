5146541184cd355b6e61563ac4b85cd4cee76956
Dario Ocles <Dario Ocles>
2026-02-04 13:32:29 +0100

    fix: test suite

--- mail-sender/src/middleware/http.middleware.js
+++ mail-sender/src/middleware/http.middleware.js
@@ -3,6 +3,9 @@ import readConfiguration from '../utils/config.utils.js';
 /**
  * Configure Middleware. Example only. Adapt on your own
  */
-export const httpMiddlewareOptions = {
-  host: `https://api.${readConfiguration().region}.commercetools.com`,
+export const getHttpMiddlewareOptions = () => {
+  const config = readConfiguration();
+  return {
+    host: `https://api.${config.region}.commercetools.com`,
+  };
 };
